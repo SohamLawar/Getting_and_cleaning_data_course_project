af331fea1b16728be75946032ad7854191447813
SohamLawar <sohamlawar1994@gmail.com>
2016-05-22 01:28:21 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -52,22 +52,9 @@ testData <- cbind(yTest,subjectTest,xTest)
 finalData <- rbind(trainingData,testData)
 
 
-# Create a new table, finalDataNoActivityType without the activityType column
-finalDataNoActivityType  = finalData[,names(finalData) != 'activityType'];
-
-# Summarizing the finalDataNoActivityType table to include just the mean of each variable for each activity and each subject
-tidyData    <- aggregate(finalDataNoActivityType[,names(finalDataNoActivityType) != c('activityId','subjectId')],by=list(activityId=finalDataNoActivityType$activityId,subjectId = finalDataNoActivityType$subjectId),mean);
-
-# Merging the tidyData with activityType to include descriptive acitvity names
-tidyData    <- merge(tidyData,activityType,by='activityId',all.x=TRUE);
-
-# Export the tidyData set 
-write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t')
-
-
 # Create a second, independent tidy data set with the average of each variable
 # for each activity and each subject
 # first two columns (activity & subject)
 averages_data <- ddply(finalData, .(subjectId, activityId), function(x) colMeans(x[, 3:81]))
 
-write.table(averages_data, "averages_data.txt", row.name=FALSE)
+write.table(averages_data, "tidy_data.txt", row.name=FALSE)
